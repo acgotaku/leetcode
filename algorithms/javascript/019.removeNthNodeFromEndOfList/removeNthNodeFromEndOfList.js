c4a95c68fcb2cfec8c813d35ed19079c71e6ac37
acgotaku <acgotaku311@gmail.com>
2018-01-25 23:32:14 +0900

    add code

---
+++ algorithms/javascript/019.removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.js
@@ -0,0 +1,58 @@
+// Source : https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+// Author : acgotaku311
+// Date   : 2018-01-25
+
+/**********************************************************************************
+*
+* Given a linked list, remove the nth node from the end of list and return its head.
+*
+* For example,
+*
+*    Given linked list: 1->2->3->4->5, and n = 2.
+*
+*    After removing the second node from the end, the linked list becomes 1->2->3->5.
+*
+* Note:
+* Given n will always be valid.
+* Try to do this in one pass.
+*
+*
+**********************************************************************************/
+
+function ListNode (val) {
+  this.val = val
+  this.next = null
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = function (head, n) {
+  if (head === null || n < 0) {
+    return null
+  }
+  let fakeHead = new ListNode(-1)
+  fakeHead.next = head
+  head = fakeHead
+  let p1 = new ListNode(-1)
+  let p2 = new ListNode(-1)
+  p1.next = head
+  p2.next = head
+  for (let i = 0; i < n; i++) {
+    if (p2 === null) {
+      return null
+    }
+    p2 = p2.next
+  }
+
+  while (p2.next !== null) {
+    p2 = p2.next
+    p1 = p1.next
+  }
+  p1.next = p1.next.next
+  return head.next
+}
+
+export { ListNode, removeNthFromEnd }
---
+++ algorithms/javascript/019.removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.spec.js
@@ -0,0 +1,19 @@
+import { ListNode, removeNthFromEnd } from './removeNthNodeFromEndOfList'
+
+describe('removeNthNodeFromEndOfList', function () {
+  const reducer = (accumulator, currentValue) => {
+    const node = new ListNode(currentValue)
+    node.next = accumulator
+    return node
+  }
+  it('removeNthNodeFromEndOfList test case 1', function () {
+    const head = [1, 2, 3, 4, 5].reduceRight(reducer, null)
+    const result = [1, 2, 3, 5].reduceRight(reducer, null)
+    expect(removeNthFromEnd(head, 2)).toEqual(result)
+  })
+  it('removeNthNodeFromEndOfList test case 1', function () {
+    const head = [1, 2, 3, 4, 5].reduceRight(reducer, null)
+    const result = [1, 2, 3, 4].reduceRight(reducer, null)
+    expect(removeNthFromEnd(head, 1)).toEqual(result)
+  })
+})
