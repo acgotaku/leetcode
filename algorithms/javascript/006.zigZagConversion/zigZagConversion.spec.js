1033bca6b5de0a13824379675055195dbab9182b
acgotaku <acgotaku311@gmail.com>
2019-12-05 22:48:22 +0900

    add test case

--- algorithms/javascript/006.zigZagConversion/zigZagConversion.spec.js
+++ algorithms/javascript/006.zigZagConversion/zigZagConversion.spec.js
@@ -1,10 +1,16 @@
 import { convert } from './zigZagConversion'
 
 describe('zigZagConversion', function () {
-  it('convert', function () {
+  it('convert test case 1', function () {
     const s = 'PAYPALISHIRING'
-    const numRows = '3'
+    const numRows = 3
     const result = 'PAHNAPLSIIGYIR'
+    expect(convert(s, numRows)).toBe(result)
+  })
+  it('convert test case 2', function () {
+    const s = 'HELLO'
+    const numRows = 1
+    const result = 'HELLO'
     expect(convert(s, numRows)).toBe(result)
   })
 })
