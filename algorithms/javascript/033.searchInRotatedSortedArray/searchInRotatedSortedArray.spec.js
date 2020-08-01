6238c6def9c53b31941a86e6e0368cc747041e17
acgotaku <acgotaku311@gmail.com>
2020-08-01 20:30:29 +0900

    add no 33

--- algorithms/javascript/033.searchInRotatedSortedArray/searchInRotatedSortedArray.spec.js
+++ algorithms/javascript/033.searchInRotatedSortedArray/searchInRotatedSortedArray.spec.js
@@ -6,4 +6,19 @@ describe('search', function () {
     const target = 1
     expect(search(nums, target)).toEqual(5)
   })
+  it('search test case 2', function () {
+    const nums = [10, 5, 6, 7, 8, 9]
+    const target = 8
+    expect(search(nums, target)).toEqual(4)
+  })
+  it('search test case 3', function () {
+    const nums = [10, 5, 6, 7, 8, 9]
+    const target = 11
+    expect(search(nums, target)).toEqual(-1)
+  })
+  it('search test case 4', function () {
+    const nums = [6, 7, 8, 9, 10, 11, 12, 13, 5]
+    const target = 7
+    expect(search(nums, target)).toEqual(1)
+  })
 })
