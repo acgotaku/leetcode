16ded53686bb3e75c4aaf5411d3e02145304db57
acgotaku <acgotaku311@gmail.com>
2020-08-01 16:59:51 +0900

    add no 30

--- algorithms/javascript/030.substringWithConcatenationOfAllWords/substringWithConcatenationOfAllWords.spec.js
+++ algorithms/javascript/030.substringWithConcatenationOfAllWords/substringWithConcatenationOfAllWords.spec.js
@@ -1,10 +1,28 @@
 import { findSubstring } from './substringWithConcatenationOfAllWords'
 
 describe('substringWithConcatenationOfAllWords', function () {
-  it('substringWithConcatenationOfAllWords', function () {
+  it('substringWithConcatenationOfAllWords test case 1', function () {
     const s = 'barfoothefoobarman'
     const words = ['foo', 'bar']
     const result = [0, 9]
+    expect(findSubstring(s, words)).toEqual(result)
+  })
+  it('substringWithConcatenationOfAllWords test case 2', function () {
+    const s = 'barfoothefoobarman'
+    const words = []
+    const result = []
+    expect(findSubstring(s, words)).toEqual(result)
+  })
+  it('substringWithConcatenationOfAllWords test case 3', function () {
+    const s = 'barbarfoofoobar'
+    const words = ['foo', 'bar']
+    const result = [3, 9]
+    expect(findSubstring(s, words)).toEqual(result)
+  })
+  it('substringWithConcatenationOfAllWords test case 4', function () {
+    const s = 'foobarbarfoofoobar'
+    const words = ['foo', 'bar', 'foo']
+    const result = [6, 9]
     expect(findSubstring(s, words)).toEqual(result)
   })
 })
