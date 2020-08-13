c61563dab85444f951e9e66b380cd2f918e1c933
acgotaku <acgotaku311@gmail.com>
2020-08-13 23:47:11 +0900

    add test case

--- algorithms/javascript/060.permutationSequence/permutationSequence.spec.js
+++ algorithms/javascript/060.permutationSequence/permutationSequence.spec.js
@@ -4,4 +4,7 @@ describe('getPermutation', function () {
   it('getPermutation test case 1', function () {
     expect(getPermutation(3, 5)).toEqual('312')
   })
+  it('getPermutation test case 2', function () {
+    expect(getPermutation(3, 8)).toEqual('')
+  })
 })
