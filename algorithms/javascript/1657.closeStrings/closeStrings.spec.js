34d52538094d9c2c1b5a033d0aa87d9516ce57d8
acgotaku <acgotaku311@gmail.com>
2020-11-25 23:20:26 +0900

    add test case

--- algorithms/javascript/1657.closeStrings/closeStrings.spec.js
+++ algorithms/javascript/1657.closeStrings/closeStrings.spec.js
@@ -25,4 +25,10 @@ describe('closeStrings', function () {
 
     expect(closeStrings(word1, word2)).toEqual(false)
   })
+  it('test case 5', function () {
+    const word1 = 'abbzzca'
+    const word2 = 'babzzcz'
+
+    expect(closeStrings(word1, word2)).toEqual(false)
+  })
 })
