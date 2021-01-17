a160ad7128be0a2100296c0ffefa5933b16071b1
acgotaku <acgotaku311@gmail.com>
2021-01-17 22:41:38 +0900

    fix code

--- algorithms/javascript/1726.tupleSameProduct/tupleSameProduct.js
+++ algorithms/javascript/1726.tupleSameProduct/tupleSameProduct.js
@@ -10,29 +10,18 @@ const tupleSameProduct = function (nums) {
   let count = 0
   const len = nums.length
   const dict = {}
-  const factorial = (n) => {
-    let val = 1
-    for (let i = 2; i <= n; i++) { val = val * i }
-    return val
-  }
   for (let i = 0; i < len - 1; i++) {
     for (let j = i + 1; j < len; j++) {
       const res = nums[i] * nums[j]
       if (dict[res]) {
-        dict[res].push([nums[i], nums[j]])
+        dict[res]++
       } else {
-        dict[res] = [[nums[i], nums[j]]]
+        dict[res] = 1
       }
+      count += 8 * (dict[res] - 1)
     }
   }
 
-  for (const key in dict) {
-    const arr = dict[key]
-    if (arr.length > 1) {
-      const len = arr.length
-      count += (factorial(len) / (2 * factorial(len - 2)) * 8)
-    }
-  }
   return count
 }
 
