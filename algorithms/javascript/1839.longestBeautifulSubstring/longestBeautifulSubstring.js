06cf58be7e28eaa6e31a2ec6d6baca5a8f827857
acgotaku <acgotaku311@gmail.com>
2021-04-25 23:09:21 +0900

    fix 1839

--- algorithms/javascript/1839.longestBeautifulSubstring/longestBeautifulSubstring.js
+++ algorithms/javascript/1839.longestBeautifulSubstring/longestBeautifulSubstring.js
@@ -8,27 +8,18 @@
  */
 const longestBeautifulSubstring = function (word) {
   const len = word.length
-  const vowels = ['a', 'e', 'i', 'o', 'u']
+
   let dict = {}
   let l = 0
   let count = 0
   for (let r = 0; r < len; r++) {
-    if (vowels.includes(word[r])) {
-      if (r > 0) {
-        if (word[r] < word[r - 1]) {
-          l = r
-          dict = {}
-          dict[word[r]] = true
-          continue
-        }
+    if (r > 0) {
+      if (word[r] < word[r - 1]) {
+        l = r
+        dict = {}
       }
-      dict[word[r]] = true
-    } else {
-      l = r
-      dict = {}
-      dict[word[r]] = true
-      continue
     }
+    dict[word[r]] = true
     if (word[l] === 'a' && word[r] === 'u' && r - l + 1 >= 5) {
       if (Object.keys(dict).length >= 5) {
         count = Math.max(count, r - l + 1)
